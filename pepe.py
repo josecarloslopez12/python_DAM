b426352f21d6496a97adaea9b4d2481bb89d5197
josecarloslopez12 <j.carlolopezgalle.com>
2025-10-22 19:38:22 +0200

    elpepe

--- pepe.py
+++ pepe.py
@@ -1,1 +1,4 @@
 print("aaa  bbb ccccc           ddd ee  f   gggg    hh h iii jjj  k kk lll mmmm n n ooo ppp qqq r r sss ttt u u v v w w x  y  zzz   ")
+print("a a b b c     d d e  f   g       hhh   i   j   k k  l   m m m n n o o p p q q r r s     t   u u v v w w  x   y    z    ")
+print("aaa  bbb ccccc ddd ee  f   gggg     h h iii jjj  k kk lll m m m n n ooo ppp qqq r r sss    t   uuu  v   w w x  y  zzz   ")
+print("elpepe")
